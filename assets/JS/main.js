fec02e1b9dabf18f0ca69d19de0c34b6327f6ed8
Moamen777 <moamendev77@gmail.com>
2023-11-04 14:38:14 +0200

    Project 13

---
+++ assets/JS/main.js
@@ -0,0 +1,14 @@
+var number =Number(window.prompt("enter number"));
+if(number > 0)
+{
+    window.alert("the number is positive");
+    console.log("the number is positive");
+}else if(number < 0)
+{
+    window.alert("the number is negative");
+    console.log("the number is negative");
+}else
+{
+    window.alert("the number is 0");
+    console.log("the number is 0");
+}
